c2f9f7fea18d0eb0aa68f67f0d00aa9a3212619c
bpinheiro <bernardopinheiro19@gmail.com>
2024-12-22 19:50:34 +0000

    Finished solution for day3

--- 2024/day3/day3.go
+++ 2024/day3/day3.go
@@ -25,6 +25,31 @@ func checkRegEx(input string, regex string) []string {
 	return pattern.FindAllString(input, -1)
 }
 
+func multiplyValues(mul string) int {
+	result := 0
+	sumList := checkRegEx(mul, `(\d*\,\d*)`)
+
+	for _, val := range sumList {
+		splitSumList := strings.Split(val, ",")
+
+		num1, err := strconv.Atoi(splitSumList[0])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		num2, err := strconv.Atoi(splitSumList[1])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result += num1 * num2
+	}
+
+	return result
+}
+
 func day3Part1() int {
 	result := 0
 
@@ -34,26 +59,7 @@ func day3Part1() int {
 		mulList := checkRegEx(line, `(mul\(\d*,\d*\))`)
 
 		for _, mul := range mulList {
-			sumList := checkRegEx(mul, `(\d*\,\d*)`)
-
-			for _, val := range sumList {
-				splitSumList := strings.Split(val, ",")
-
-				num1, err := strconv.Atoi(splitSumList[0])
-
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				num2, err := strconv.Atoi(splitSumList[1])
-
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				result += num1 * num2
-			}
-
+			result += multiplyValues(mul)
 		}
 
 	}
@@ -63,6 +69,30 @@ func day3Part1() int {
 
 func day3Part2() int {
 	result := 0
+	mulEnabled := true
+
+	list := getStringFromFile("input.txt")
+
+	for _, line := range list {
+
+		firstList := checkRegEx(line, `(mul\(\d*,\d*\))|(do\(\))|(don't\(\))`)
+
+		for _, val := range firstList {
+
+			if val == "do()" {
+				mulEnabled = true
+			} else if val == "don't()" {
+				mulEnabled = false
+			} else {
+
+				if mulEnabled {
+					result += multiplyValues(val)
+				}
+			}
+
+		}
+
+	}
 
 	return result
 }
