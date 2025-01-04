0a14a91f2e8bdb0b9b86529fc70945e75220323f
bpinheiro <bernardopinheiro19@gmail.com>
2025-01-04 00:52:07 +0000

    Minor structure fix

---
+++ 2024/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"aoc2024/day1"
+	"aoc2024/day2"
+	"aoc2024/day3"
+	"aoc2024/day4"
+	"aoc2024/day5"
+	"aoc2024/day6"
+	"aoc2024/day7"
+)
+
+func main() {
+	day1.Run("day1/input.txt")
+	day2.Run("day2/input.txt")
+	day3.Run("day3/input.txt")
+	day4.Run("day4/input.txt")
+	day5.Run("day5/input.txt")
+	day6.Run("day6/input.txt")
+	day7.Run("day7/input.txt")
+}
