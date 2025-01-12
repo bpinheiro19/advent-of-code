ee589ba94fc8446b5e97664c6fed4220bdb16d2a
bpinheiro <bernardopinheiro19@gmail.com>
2025-01-12 15:27:41 +0000

    Day10 Part2 Solution

--- 2024/day10/day10_test.go
+++ 2024/day10/day10_test.go
@@ -8,7 +8,7 @@ const FILENAME string = "input.txt"
 const TEST_FILENAME string = "test_input.txt"
 
 func TestDay10Part1Example(t *testing.T) {
-	result := day10Part1(TEST_FILENAME)
+	result, _ := day10(TEST_FILENAME)
 	expectedResult := 36
 	if result != expectedResult {
 		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
@@ -16,15 +16,15 @@ func TestDay10Part1Example(t *testing.T) {
 }
 
 func TestDay10Part2Example(t *testing.T) {
-	result := day10Part2(TEST_FILENAME)
-	expectedResult := 0
+	_, result := day10(TEST_FILENAME)
+	expectedResult := 81
 	if result != expectedResult {
 		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
 	}
 }
 
 func TestDay10Part1(t *testing.T) {
-	result := day10Part1(FILENAME)
+	result, _ := day10(FILENAME)
 	expectedResult := 820
 	if result != expectedResult {
 		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
@@ -32,8 +32,8 @@ func TestDay10Part1(t *testing.T) {
 }
 
 func TestDay10Part2(t *testing.T) {
-	result := day10Part2(FILENAME)
-	expectedResult := 0
+	_, result := day10(FILENAME)
+	expectedResult := 1786
 	if result != expectedResult {
 		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
 	}
