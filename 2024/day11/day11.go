cb75fd7184678d4c0d9edc9d42d8f6e8bf51c157
bpinheiro <bernardopinheiro19@gmail.com>
2025-01-15 23:50:53 +0000

    Day 11 part2 solution

--- 2024/day11/day11.go
+++ 2024/day11/day11.go
@@ -50,8 +50,73 @@ func day11Part1(filename string) int {
 	return len(stoneList)
 }
 
+func createStoneMap(filename string) map[int]int {
+
+	stringList := utils.GetStringListFromFile(filename)
+
+	stoneMap := make(map[int]int)
+
+	for _, str := range stringList {
+		stoneMap[utils.StringToInt(str)] += 1
+	}
+	return stoneMap
+}
+
+func copyMap(stoneMap map[int]int) map[int]int {
+	newStoneMap := make(map[int]int, 0)
+
+	for x, i := range stoneMap {
+		newStoneMap[x] = i
+	}
+	return newStoneMap
+}
+
+func improvedBlink(stoneMap map[int]int) map[int]int {
+
+	newStoneMap := copyMap(stoneMap)
+
+	for key, val := range stoneMap {
+
+		if key == 0 {
+			newStoneMap[key] -= val
+			newStoneMap[1] += val
+
+		} else if len(utils.IntToString(key))%2 == 0 {
+
+			newStoneMap[key] -= val
+
+			str := utils.IntToString(key)
+			halfLen := len(str) / 2
+
+			newStoneMap[utils.StringToInt(str[:halfLen])] += val
+			newStoneMap[utils.StringToInt(str[halfLen:])] += val
+
+		} else {
+			newStoneMap[key] -= val
+			newStoneMap[key*2024] += val
+		}
+
+		if newStoneMap[key] <= 0 {
+			delete(newStoneMap, key)
+		}
+	}
+	return newStoneMap
+}
+
 func day11Part2(filename string) int {
-	return 0
+
+	stoneMap := createStoneMap(filename)
+
+	for i := 0; i < 75; i++ {
+		stoneMap = improvedBlink(stoneMap)
+	}
+
+	stoneLength := 0
+	for _, val := range stoneMap {
+		stoneLength += val
+	}
+
+	return stoneLength
 }
 
 func Run(filename string) {
