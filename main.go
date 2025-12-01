e98e50334edfdd6d17b0f1a054fdc1bf94cbfd75
bpinheiro <bernardopinheiro19@gmail.com>
2025-12-01 22:31:29 +0000

    Rearranged main.go

--- main.go
+++ main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
-	"github.com/bpinheiro19/advent-of-code/2024/day1"
-	"github.com/bpinheiro19/advent-of-code/2024/day10"
-	"github.com/bpinheiro19/advent-of-code/2024/day11"
-	"github.com/bpinheiro19/advent-of-code/2024/day2"
-	"github.com/bpinheiro19/advent-of-code/2024/day3"
-	"github.com/bpinheiro19/advent-of-code/2024/day4"
-	"github.com/bpinheiro19/advent-of-code/2024/day5"
-	"github.com/bpinheiro19/advent-of-code/2024/day6"
-	"github.com/bpinheiro19/advent-of-code/2024/day7"
-	"github.com/bpinheiro19/advent-of-code/2024/day8"
-	"github.com/bpinheiro19/advent-of-code/2024/day9"
+	day1_24 "github.com/bpinheiro19/advent-of-code/2024/day1"
+	day10_24 "github.com/bpinheiro19/advent-of-code/2024/day10"
+	day11_24 "github.com/bpinheiro19/advent-of-code/2024/day11"
+	day2_24 "github.com/bpinheiro19/advent-of-code/2024/day2"
+	day3_24 "github.com/bpinheiro19/advent-of-code/2024/day3"
+	day4_24 "github.com/bpinheiro19/advent-of-code/2024/day4"
+	day5_24 "github.com/bpinheiro19/advent-of-code/2024/day5"
+	day6_24 "github.com/bpinheiro19/advent-of-code/2024/day6"
+	day7_24 "github.com/bpinheiro19/advent-of-code/2024/day7"
+	day8_24 "github.com/bpinheiro19/advent-of-code/2024/day8"
+	day9_24 "github.com/bpinheiro19/advent-of-code/2024/day9"
+	day1_25 "github.com/bpinheiro19/advent-of-code/2025/day1"
 )
 
 func main() {
-	day1.Run("2024/day1/input.txt")
-	day2.Run("2024/day2/input.txt")
-	day3.Run("2024/day3/input.txt")
-	day4.Run("2024/day4/input.txt")
-	day5.Run("2024/day5/input.txt")
-	day6.Run("2024/day6/input.txt")
-	day7.Run("2024/day7/input.txt")
-	day8.Run("2024/day8/input.txt")
-	day9.Run("2024/day9/input.txt")
-	day10.Run("2024/day10/input.txt")
-	day11.Run("2024/day11/input.txt")
+	AOC2025()
+}
+
+func AOC2025() {
+	day1_25.Run("2025/day1/test-input.txt")
+}
+
+func AOC2024() {
+	day1_24.Run("2024/day1/input.txt")
+	day2_24.Run("2024/day2/input.txt")
+	day3_24.Run("2024/day3/input.txt")
+	day4_24.Run("2024/day4/input.txt")
+	day5_24.Run("2024/day5/input.txt")
+	day6_24.Run("2024/day6/input.txt")
+	day7_24.Run("2024/day7/input.txt")
+	day8_24.Run("2024/day8/input.txt")
+	day9_24.Run("2024/day9/input.txt")
+	day10_24.Run("2024/day10/input.txt")
+	day11_24.Run("2024/day11/input.txt")
 }
