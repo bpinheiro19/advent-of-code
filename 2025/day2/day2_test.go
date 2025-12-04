91ab3cd876296511af20dffbb1d23d913be157a5
bpinheiro <bernardopinheiro19@gmail.com>
2025-12-04 23:41:13 +0000

    2025 Day2 Part 1 Solution

---
+++ 2025/day2/day2_test.go
@@ -0,0 +1,37 @@
+package day2
+
+import (
+	"testing"
+)
+
+func TestDay2Part1Example(t *testing.T) {
+	result := day2Part1("test_input.txt")
+	expectedResult := 1227775554
+	if result != expectedResult {
+		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
+	}
+}
+
+func TestDay2Part2Example(t *testing.T) {
+	result := day2Part2("test_input.txt")
+	expectedResult := 0
+	if result != expectedResult {
+		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
+	}
+}
+
+func TestDay2Part1(t *testing.T) {
+	result := day2Part1("input.txt")
+	expectedResult := 17077011375
+	if result != expectedResult {
+		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
+	}
+}
+
+func TestDay2Part2(t *testing.T) {
+	result := day2Part2("input.txt")
+	expectedResult := 0
+	if result != expectedResult {
+		t.Errorf("Result was incorrect, got: %d, wanted: %d.", result, expectedResult)
+	}
+}
